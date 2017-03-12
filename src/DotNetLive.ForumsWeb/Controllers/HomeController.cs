566beee5c7ecb0475c6b26899f65bf50f4629306
Duke Cheng <dk@feinian.me>
2017-03-12 15:42:24 +0800

    Added email send demo

--- src/DotNetLive.ForumsWeb/Controllers/HomeController.cs
+++ src/DotNetLive.ForumsWeb/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace DotNetLive.ForumsWeb.Controllers
 {
@@ -29,6 +31,44 @@ namespace DotNetLive.ForumsWeb.Controllers
 
         public IActionResult Error()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MailTest()
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Duke Cheng", "duke@feinian.me"));
+            message.To.Add(new MailboxAddress("Duke Cheng", "duke.cheng@outlook.com"));
+            message.To.Add(new MailboxAddress("FeiNian Cheng", "feinian.cheng@hotmail.com"));
+            message.Subject = "How you doin'?";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Hey Chandler,
+
+I just wanted to let you know that Monica and I were going to go play some paintball, you in?
+
+-- Joey"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client.Connect("email host", 587, false);
+
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("username", @"password");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
             return View();
         }
     }
